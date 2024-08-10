061e639a40e6bd7303843292d60ec1d59fc7ca34
John K N Murphy <j.murpheous@gmail.com>
2024-08-10 16:15:43 +1200

    First attepmt at doing a line in U#

---
+++ Scripts/UdonVolumetricLine.cs
@@ -0,0 +1,378 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VolumetricLines;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class UdonVolumetricLine : UdonSharpBehaviour
+{
+    // Used to compute the average value of all the Vector3's components:
+    private readonly Vector3 Average = new Vector3(1f / 3f, 1f / 3f, 1f / 3f);
+
+    #region private variables
+    /// <summary>
+    /// The start position relative to the GameObject's origin
+    /// </summary>
+    [SerializeField]
+    private Vector3 m_startPos;
+
+    /// <summary>
+    /// The end position relative to the GameObject's origin
+    /// </summary>
+    [SerializeField]
+    private Vector3 m_endPos = new Vector3(0f, 0f, 100f);
+
+    /// <summary>
+    /// Line Color
+    /// </summary>
+    [SerializeField]
+    private Color m_lineColor;
+
+    /// <summary>
+    /// The width of the line
+    /// </summary>
+    [SerializeField]
+    private float m_lineWidth;
+
+    /// <summary>
+    /// Light saber factor
+    /// </summary>
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_lightSaberFactor;
+
+    /// <summary>
+    /// This GameObject's specific material
+    /// </summary>
+    private Material material;
+
+    /// <summary>
+    /// This GameObject's mesh filter
+    /// </summary>
+    private MeshFilter mf;
+    private Mesh mesh;
+    [SerializeField]
+    private bool iHaveComponents = false;
+    #endregion
+
+    #region properties
+
+    /// <summary>
+    /// Get or set the line color of this volumetric line's material
+    /// </summary>
+    public Color LineColor
+    {
+        get { return m_lineColor; }
+        set
+        {
+            if (material != null)
+            {
+                m_lineColor = value;
+                material.color = m_lineColor;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get or set the line width of this volumetric line's material
+    /// </summary>
+    public float LineWidth
+    {
+        get { return m_lineWidth; }
+        set
+        {
+            if (material != null)
+            {
+                m_lineWidth = value;
+                material.SetFloat("_LineWidth", m_lineWidth);
+            }
+            UpdateBounds();
+        }
+    }
+
+    /// <summary>
+    /// Get or set the light saber factor of this volumetric line's material
+    /// </summary>
+    public float LightSaberFactor
+    {
+        get { return m_lightSaberFactor; }
+        set
+        {
+            if (material != null)
+            {
+                m_lightSaberFactor = value;
+                material.SetFloat("_LightSaberFactor", m_lightSaberFactor);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get or set the start position of this volumetric line's mesh
+    /// </summary>
+    public Vector3 StartPos
+    {
+        get { return m_startPos; }
+        set
+        {
+            m_startPos = value;
+            SetStartAndEndPoints(m_startPos, m_endPos);
+        }
+    }
+
+    /// <summary>
+    /// Get or set the end position of this volumetric line's mesh
+    /// </summary>
+    public Vector3 EndPos
+    {
+        get { return m_endPos; }
+        set
+        {
+            m_endPos = value;
+            SetStartAndEndPoints(m_startPos, m_endPos);
+        }
+    }
+
+    #endregion
+
+    #region methods
+
+    /// <summary>
+    /// Calculates the (approximated) _LineScale factor based on the object's scale.
+    /// </summary>
+    private float CalculateLineScale()
+    {
+        return Vector3.Dot(transform.lossyScale, Average);
+    }
+
+    /// <summary>
+    /// Updates the line scaling of this volumetric line based on the current object scaling.
+    /// </summary>
+    public void UpdateLineScale()
+    {
+        if (material != null)
+        {
+            material.SetFloat("_LineScale", CalculateLineScale());
+        }
+    }
+
+    /// <summary>
+    /// Sets all material properties (color, width, light saber factor, start-, endpos)
+    /// </summary>
+    private void SetAllMaterialProperties()
+    {
+        SetStartAndEndPoints(m_startPos, m_endPos);
+
+        if (material != null)
+        {
+            material.color = m_lineColor;
+            material.SetFloat("_LineWidth", m_lineWidth);
+            material.SetFloat("_LightSaberFactor", m_lightSaberFactor);
+            UpdateLineScale();
+        }
+    }
+
+    /// <summary>
+    /// Calculate the bounds of this line based on start and end points,
+    /// the line width, and the scaling of the object.
+    /// </summary>
+    private Bounds CalculateBounds()
+    {
+        var maxWidth = Mathf.Max(transform.lossyScale.x, transform.lossyScale.y, transform.lossyScale.z);
+        var scaledLineWidth = maxWidth * LineWidth * 0.5f;
+
+        var min = new Vector3(
+            Mathf.Min(m_startPos.x, m_endPos.x) - scaledLineWidth,
+            Mathf.Min(m_startPos.y, m_endPos.y) - scaledLineWidth,
+            Mathf.Min(m_startPos.z, m_endPos.z) - scaledLineWidth
+        );
+        var max = new Vector3(
+            Mathf.Max(m_startPos.x, m_endPos.x) + scaledLineWidth,
+            Mathf.Max(m_startPos.y, m_endPos.y) + scaledLineWidth,
+            Mathf.Max(m_startPos.z, m_endPos.z) + scaledLineWidth
+        );
+        Bounds bounds = new Bounds();
+        bounds.min = min;
+        bounds.max = max;
+        return bounds;
+    }
+
+    /// <summary>
+    /// Updates the bounds of this line according to the current properties, 
+    /// which there are: start point, end point, line width, scaling of the object.
+    /// </summary>
+    public void UpdateBounds()
+    {
+        if (null != mf)
+        {
+            var mesh = mf.sharedMesh;
+            Debug.Assert(null != mesh);
+            if (null != mesh)
+            {
+                mesh.bounds = CalculateBounds();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sets the start and end points - updates the data of the Mesh.
+    /// </summary>
+    public void SetStartAndEndPoints(Vector3 startPoint, Vector3 endPoint)
+    {
+        m_startPos = startPoint;
+        m_endPos = endPoint;
+
+        Vector3[] vertexPositions = {
+                m_startPos,
+                m_startPos,
+                m_startPos,
+                m_startPos,
+                m_endPos,
+                m_endPos,
+                m_endPos,
+                m_endPos,
+            };
+
+        Vector3[] other = {
+                m_endPos,
+                m_endPos,
+                m_endPos,
+                m_endPos,
+                m_startPos,
+                m_startPos,
+                m_startPos,
+                m_startPos,
+            };
+
+        mesh.vertices = vertexPositions;
+        mesh.normals = other;
+        UpdateBounds();
+    }
+    #endregion
+
+    #region event functions
+
+    private bool checkComponents()
+    {
+        if (iHaveComponents)
+            return true;
+
+        if (mf == null)
+            mf = GetComponent<MeshFilter>();
+
+        if (material == null)
+        {
+            MeshRenderer mr = GetComponent<MeshRenderer>();
+            if (mr != null)
+                material = mr.material;
+        }
+        if (mf == null || material == null)
+        {
+            Debug.Log(gameObject.name + " CheckComponents no mf or material");
+            return false;
+        }
+        mesh = mf.mesh;
+        if (mesh == null)
+        {
+            Debug.Log(gameObject.name + " CheckComponents no mesh");
+            return false;
+        }
+        SetStartAndEndPoints(m_startPos, m_endPos);
+        Vector2[] v2x8 = new Vector2[8];
+        v2x8[0] = new Vector2(1.0f, 1.0f);
+        v2x8[1] = new Vector2(1.0f, 0.0f);
+        v2x8[2] = new Vector2(0.5f, 1.0f);
+        v2x8[3] = new Vector2(0.5f, 0.0f);
+        v2x8[4] = new Vector2(0.5f, 0.0f);
+        v2x8[5] = new Vector2(0.5f, 1.0f);
+        v2x8[6] = new Vector2(0.0f, 0.0f);
+        v2x8[7] = new Vector2(0.0f, 1.0f);
+
+        mesh.uv = v2x8;
+        v2x8[0] = new Vector2(1.0f, 1.0f);
+        v2x8[1] = new Vector2(1.0f, -1.0f);
+        v2x8[2] = new Vector2(0.0f, 1.0f);
+        v2x8[3] = new Vector2(0.0f, -1.0f);
+        v2x8[4] = new Vector2(0.0f, 1.0f);
+        v2x8[5] = new Vector2(0.0f, -1.0f);
+        v2x8[6] = new Vector2(1.0f, 1.0f);
+        v2x8[7] = new Vector2(1.0f, -1.0f);
+
+        mesh.uv2 = v2x8;
+        int[] indices = new int[18];
+        // 2, 1, 0,
+        indices[0] = 2; indices[1] = 1; indices[2] = 0;
+        // 3, 1, 2,
+        indices[3] = 3; indices[4] = 1; indices[2] = 2;
+        // 4, 3, 2,
+        indices[6] = 4; indices[7] = 3; indices[8] = 2;
+        // 5, 4, 2,
+        indices[9] = 5; indices[10] = 4; indices[11] = 2;
+        // 4, 5, 6,
+        indices[12] = 4; indices[13] = 5; indices[14] = 6;
+        // 6, 5, 7
+        indices[15] = 6; indices[16] = 5; indices[17] = 7;
+        mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+        SetAllMaterialProperties();
+        return true;
+    }
+
+
+    void Start()
+    {
+        iHaveComponents = checkComponents();
+        // TODO: Need to set vertices before assigning new Mesh to the MeshFilter's mesh property => Why?
+    }
+
+    /*
+    void OnDestroy()
+    {
+        if (null != mf)
+        {
+            if (Application.isPlaying)
+            {
+                Mesh.Destroy(mf.sharedMesh);
+            }
+            else // avoid "may not be called from edit mode" error
+            {
+                Mesh.DestroyImmediate(mf.sharedMesh);
+            }
+            mf.sharedMesh = null;
+        }
+        DestroyMaterial();
+    }
+    */
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            UpdateLineScale();
+            UpdateBounds();
+        }
+    }
+
+#if UNITY_EDITOR
+//    void OnValidate()
+//    {
+ //       checkComponents();
+        // This function is called when the script is loaded or a value is changed in the inspector (Called in the editor only).
+        //  => make sure, everything stays up-to-date
+//if (string.IsNullOrEmpty(gameObject.scene.name) || string.IsNullOrEmpty(gameObject.scene.path))
+ //       {
+  //          return; // ...but not if a Prefab is selected! (Only if we're using it within a scene.)
+  //      }
+  //      CreateMaterial();
+  //      SetAllMaterialProperties();
+   //     UpdateBounds();
+    //}
+    
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(gameObject.transform.TransformPoint(m_startPos), gameObject.transform.TransformPoint(m_endPos));
+        }
+ 
+#endif
+
+#endregion
+}
---
+++ Shader/SingleLine-LightSaber.shader
@@ -0,0 +1,159 @@
+/// Render a single volumetric line using an additive shader which does not support changing the color
+/// 
+/// Based on the Volumetric lines algorithm by Sebastien Hillaire
+/// http://sebastien.hillaire.free.fr/index.php?option=com_content&view=article&id=57&Itemid=74
+/// 
+/// Thread in the Unity3D Forum:
+/// http://forum.unity3d.com/threads/181618-Volumetric-lines
+/// 
+/// Unity3D port by Johannes Unterguggenberger
+/// johannes.unterguggenberger@gmail.com
+/// 
+/// Thanks to Michael Probst for support during development.
+/// 
+/// Thanks for bugfixes and improvements to Unity Forum User "Mistale"
+/// http://forum.unity3d.com/members/102350-Mistale
+/// 
+/// Shader code optimization and cleanup by Lex Darlog (aka DRL)
+/// http://forum.unity3d.com/members/lex-drl.67487/
+/// 
+/// Single Pass Stereo Support by Unity Forum User "Abnormalia_"
+/// https://forum.unity.com/members/abnormalia_.356336/ 
+///
+/// Single pass instanced rendering fix by Niel 
+/// 
+Shader "Lines/SingleLine-LightSaber" {
+	Properties {
+		[NoScaleOffset] _MainTex ("Base (RGB)", 2D) = "white" {}
+		_LineWidth ("Line Width", Range(0.01, 100)) = 1.0
+		_LineScale ("Line Scale", Float) = 1.0
+		_LightSaberFactor ("LightSaberFactor", Range(0.0, 1.0)) = 0.9
+		[MaterialToggle] _UvBasedLightSaberFactor("UV-Based Light Saber Calculation (Anti-Aliased)", Int) = 0
+		_Color ("Main Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		// batching is forcefully disabled here because the shader simply won't work with it:
+		Tags {
+			"DisableBatching"="True"
+			"RenderType"="Transparent"
+			"Queue"="Transparent"
+			"IgnoreProjector"="True"
+			"ForceNoShadowCasting"="True"
+			"PreviewType"="Plane"
+		}
+		LOD 200
+		
+		Pass {
+			
+			Cull Off
+			ZWrite Off
+			ZTest LEqual
+			Blend One One
+			Lighting Off
+			
+			CGPROGRAM
+				#pragma glsl_no_auto_normalization
+				#pragma vertex vert
+				#pragma fragment frag
+				
+				// tell the cginc file that this is a simplified version of the shader:
+				#define LIGHT_SABER_MODE_ON
+
+			#include "UnityCG.cginc"
+	
+			// Property-variables declarations
+			sampler2D _MainTex;
+			float _LineWidth;
+			float _LineScale;
+
+			fixed _LightSaberFactor;
+			int _UvBasedLightSaberFactor;
+			fixed4 _Color;
+
+			// Vertex shader input attributes
+			struct a2v
+			{
+				float4 vertex : POSITION;
+				float3 otherPos : NORMAL; // object-space position of the other end
+				half2 texcoord : TEXCOORD0;
+				float2 texcoord1 : TEXCOORD1;
+
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+	
+			// Vertex out/fragment in data:
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+					UNITY_VERTEX_OUTPUT_STEREO
+			};
+	
+			// Vertex shader
+			v2f vert (a2v v)
+			{
+				v2f o;
+
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_INITIALIZE_OUTPUT(v2f, o);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+
+				// Pass on texture coordinates to fragment shader as they are:
+				o.uv = v.texcoord;
+		
+				// Transform to homogeneous clip space:
+				float4 csPos = UnityObjectToClipPos(v.vertex);
+				float4 csPos_other = UnityObjectToClipPos(float4(v.otherPos, 1.0));
+
+				// Scale to properly match Unity's world space units:
+				// The `projScale` factor also handles different field of view values, which 
+				// used to be handled via FOV_SCALING_OFF in previous versions of this asset.
+				// Furthermore, `projScale` handles orthographic projection matrices gracefully.
+				float projScale = unity_CameraProjection._m11 * 0.5;
+				float scaledLineWidth = _LineWidth * _LineScale * projScale;
+
+				float aspectRatio = unity_CameraProjection._m11 / unity_CameraProjection._m00;
+				// The line direction in (aspect-ratio corrected) clip space (and scaled by witdh):
+				float2 lineDirProj = normalize(
+					csPos.xy * aspectRatio / csPos.w - // screen-space pos of current end
+					csPos_other.xy * aspectRatio / csPos_other.w // screen-space position of the other end
+				) * sign(csPos.w) * sign(csPos_other.w) * scaledLineWidth;
+		
+				// Offset for our current vertex:
+				float2 offset =
+					v.texcoord1.x * lineDirProj +
+					v.texcoord1.y * float2(lineDirProj.y, -lineDirProj.x)
+				;
+
+				// Apply (aspect-ratio corrected) offset
+				csPos.x += offset.x / aspectRatio;
+				csPos.y += offset.y;
+				o.pos = csPos;
+
+				return o;
+			}
+	
+			// Fragment shader
+			fixed4 frag(v2f i) : SV_Target
+			{
+				UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(i);
+				fixed4 tx = tex2D(_MainTex, i.uv);
+
+				if (_UvBasedLightSaberFactor == 1) 
+				{
+					float2 uv2 = i.uv * 2.0 - 1.0;
+					float c = sqrt(uv2[0] * uv2[0] + uv2[1] * uv2[1]);
+					return lerp(tx * _Color, float4(1.0, 1.0, 1.0, tx.a), clamp((1.02 - c - _LightSaberFactor) * 100.0, 0, 1));
+				}
+				else 
+				{
+					return tx.a > _LightSaberFactor ? float4(1.0, 1.0, 1.0, tx.a) : tx * _Color;
+				}
+			}
+			ENDCG
+		}
+	}
+	FallBack "Diffuse"
+}
